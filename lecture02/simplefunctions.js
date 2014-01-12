9053c83d75b616166cc26f0a964e4700fb0062b7
legoodfellow <legoodfellow@gmail.com>
2014-01-12 14:23:31 -0500

    Added date and time js example file

--- lecture02/simplefunctions.js
+++ lecture02/simplefunctions.js
@@ -1,3 +1,13 @@
+// This module contains simple examples of functions that print a few things on the screen,
+// used as examples in the second lecture of cs1.
+
+// Print the current date and time.
+function print_date_and_time(){
+    var today = new Date();
+    console.log(today);
+}
+
+// Print the square root of a number.
 function print_sqrt(number){
     // Change to print once we have the correct wrapping
     // print(Math.sqrt(number));
