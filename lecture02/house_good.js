2aa06747d200ec11aa62d0ef3dd7f852f8556264
endevor <endevor4evr@gmail.com>
2014-01-14 10:57:14 -0500

    updated house_good

--- lecture02/house_good.js
+++ lecture02/house_good.js
@@ -1,29 +1,32 @@
 // Program to draw a simple house
 // cs1 example of use of commenting and whitespace
-// author Devin Balkcom
-// July, 2011
+// author Devin Balkcom (adapted from python to javascript by L'team)
+// Winter, 2014
 
 // Function to draw a house.
 function drawHouse(){
 	//draw the lawn
-	canvas.fillStyle = "#006400"; //dark green
-	canvas.fillRect(0, 100, 300, 50);
+	processing.fill(0,128,0); //dark green
+	processing.rect(0, 300, 400, 100);
 
 	//draw the main square part of the house
-	canvas.fillStyle = "#8B4513"; //brown
-	canvas.fillRect(100, 50, 100, 80);
+	processing.fill(128, 102, 51); //brown
+	processing.rect(200, 250, 130, 100);
 
 	//draw the roof
-	canvas.fillStyle = "#8B0000";//red
-	canvas.fillRect(90,30, 120,20);
+	processing.fill(255,0,0);//red
+	processing.triangle(200, 250, 330, 250, 265, 20);
 
 	// draw the door
-	canvas.fillStyle = "#B8860B"; // light brown
-	canvas.fillRect(140, 80, 20, 50);
+	processing.fill(153,128,77); // light brown
+	processing.rect(280, 310, 25, 40);
 
 	//draw the doorknob
-	canvas.fillStyle = "#000000"; // black
-	canvas.fillRect(150, 100, 5, 5);
+	processing.fill(0,0,0); // black
+	ellipse(300, 330, 3,3);
 }
+
+//set the size of the window
+size(400,400);
 
 drawHouse();
