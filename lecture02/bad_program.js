f543f6f2d2fe2c995c459dceaf7bc267c58f79ea
endevor <endevor4evr@gmail.com>
2014-01-14 11:01:12 -0500

    updated bad_program

--- lecture02/bad_program.js
+++ lecture02/bad_program.js
@@ -1,12 +1,13 @@
 function whatever(){
-	canvas.fillStyle = "#006400";
-	canvas.fillRect(0, 100, 300, 50);
-	canvas.fillStyle = "#8B4513"; 
-	canvas.fillRect(100, 50, 100, 80);
-	canvas.fillStyle = "#8B0000";
-	canvas.fillRect(90,30, 120,20);
-	canvas.fillStyle = "#B8860B";
-	canvas.fillRect(140, 80, 20, 50);
-	canvas.fillStyle = "#000000";
-	canvas.fillRect(150, 100, 5, 5);}
+processing.fill(0,128,0); 
+processing.rect(0, 300, 400, 100);
+processing.fill(128, 102, 51); 
+processing.rect(200, 250, 130, 100);
+processing.fill(255,0,0);
+processing.triangle(200, 250, 330, 250, 265, 20);
+processing.fill(153,128,77); 
+processing.rect(280, 310, 25, 40);
+processing.fill(0,0,0);
+ellipse(300, 330, 3,3);}
+size(400,400);
 whatever();
