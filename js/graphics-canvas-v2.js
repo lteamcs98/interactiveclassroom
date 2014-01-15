d429fd80c96c88fe2b12995a1a272f94a14794f2
tevnpowers <tevnpowers@gmail.com>
2014-01-15 02:07:20 -0500

    working wrapper w/ auto-looping disabled and new UI

--- js/graphics-canvas-v2.js
+++ js/graphics-canvas-v2.js
@@ -1,25 +1,10 @@
 /* graphics-canvas.js
- * Manages HTML5 canvas interaction
+ * Manages communication between CodeMirror console and HTML5 canvas
  */
-
-$(document).ready(function() {
-	$("#compile-button").click(evalCode);
-});
 
 var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("editor"), {
     lineNumbers: true,
     mode: "text/javascript",
     matchBrackets: true,
-    firstLineNumber: 0,
-	viewportMargin: Infinity
-}); 
-
-var evalCode = function() {
-	var canvas = $("#canvas")[0].getContext("2d");
-	// Clear the canvas
-	canvas.fillStyle="#FFFFFF";
-	canvas.fillRect(0, 0, $("#canvas")[0].width, $("#canvas")[0].height);
-	var code = myCodeMirror.getValue();
-	console.log(code);
-	eval(code);
-}
+    firstLineNumber: 0
+});
