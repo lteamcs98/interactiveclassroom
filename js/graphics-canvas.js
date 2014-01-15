b4760f6f7c9a64403840d70a00a615693f815311
tevnpowers <tevnpowers@gmail.com>
2014-01-14 21:31:53 -0500

    more functionality in the wrapper class!

--- js/graphics-canvas.js
+++ js/graphics-canvas.js
@@ -2,23 +2,9 @@
  * Manages communication between CodeMirror console and HTML5 canvas
  */
 
-$(document).ready(function() {
-	$("#compile-button").click(evalCode);
-});
-
 var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("console"), {
     lineNumbers: true,
     mode: "text/javascript",
     matchBrackets: true,
     firstLineNumber: 0
-}); 
-
-var evalCode = function() {
-	var canvas = $("#canvas")[0].getContext("2d");
-	// Clear the canvas
-	canvas.fillStyle="#FFFFFF";
-	canvas.fillRect(0, 0, $("#canvas")[0].width, $("#canvas")[0].height);
-	var code = myCodeMirror.getValue();
-	console.log(code);
-	eval(code);
-}
+});
