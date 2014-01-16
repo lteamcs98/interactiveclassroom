3080a3ea9ab78ee567bf97c3cd36932258f9bebd
Michelle Shu <shu.michelle.w@gmail.com>
2014-01-16 12:02:21 -0500

    remove old files

--- lecture02/simple_graphics.js
+++
@@ -1,7 +0,0 @@
-// We must make sure we have all the right wrapper functions for this to work:
-// clear and draw_circle
-    
-clear();
-    
-draw_circle(125, 100, 50);
-draw_circle(275, 100, 50);
