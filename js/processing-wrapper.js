06c9bca59f4351264a05c3a6beed9f46c10f5f0f
tevnpowers <tevnpowers@gmail.com>
2014-01-15 23:27:00 -0500

    added more wrapper functions. added a stop sketch button

--- js/processing-wrapper.js
+++ js/processing-wrapper.js
@@ -55,8 +55,8 @@ function wrapperClass(processing, code)
         var interval_ids = [];
         
         // add callbacks so that the sketch will be stopped 
-        $("#stop-button").click(stopSketch);
         $("#compile-button").click(stopSketch);
+        $("#stop-button").click(stopSketch);
         
         console.log('initialized wrapper class');
         
@@ -119,6 +119,16 @@ function wrapperClass(processing, code)
         {
             processing.stroke(r, g, b, alpha);
         }
+
+        function noStroke()
+        {
+            processing.noStroke();
+        }
+        
+        function fill(r, g, b)
+        {
+            processing.fill(r, g, b);
+        }
         
         // draw a line between the points with the given coordinates
         function line(x1, y1, x2, y2)
@@ -137,7 +147,7 @@ function wrapperClass(processing, code)
         {
             return processing.height;
         }
-        
+               
         // tell processing not to loop on the draw function
         function noLoop()
         {
@@ -181,17 +191,20 @@ function wrapperClass(processing, code)
             // exit the current sketch
             exit();
             
+            /*
             // get the canvas
             var canvas = $("#canvas1")[0].getContext("2d");
             
             // Clear the canvas
             canvas.fillStyle="#FFFFFF";
             canvas.fillRect(0, 0, $("#canvas1")[0].width, $("#canvas1")[0].height);
+            */
         }
         
         // evaluate/run the code the user writes 
         this.evalCode = function()
         {
+            stopSketch();
             noLoop();
 			
 			console.log(this.code);
@@ -213,74 +226,9 @@ frameRate(30);
 strokeWeight(12);
 
 // draw
-processing.draw = function()
-{
-    stroke(processing.random(50), processing.random(255), processing.random(255), 100);
-    line(i, 0, random(0, width()), height());
-    line(width()-i, 0, random(0, width()), height());
-    line(i, height(), random(0, width()), 0);
-    line(width()-i, height(), random(0, width()), 0);            
-    if(i < width())
-    {
-        i = i + 1;
-    } else
-    {
-        i = 0;
-    }
-    ellipse(random(0, width()), random(0, height()), 25, 25);
-  	triangle(0, 0, width(), 0, width()/2, height()/2);
-  	triangle(0, height(), width(), height(), width()/2, height()/2);
-}
-*/
-
-/*
-// sketch code
-var i = 0;
-
-// set up
-background(255);
-size(750, 250);
-smooth();
-frameRate(30);
-strokeWeight(12);
-noLoop();
-
-// draw
-draw()
-{
-    stroke(processing.random(50), processing.random(255), processing.random(255), 100);
-    line(i, 0, random(0, width()), height());
-    line(width()-i, 0, random(0, width()), height());
-    line(i, height(), random(0, width()), 0);
-    line(width()-i, height(), random(0, width()), 0);            
-    if(i < width())
-    {
-        i = i + 1;
-    } else
-    {
-        i = 0;
-    }
-    ellipse(random(0, width()), random(0, height()), 25, 25);
-  	triangle(0, 0, width(), 0, width()/2, height()/2);
-  	triangle(0, height(), width(), height(), width()/2, height()/2);
-}
-*/
-
-/*
-// sketch code
-var i = 0;
-
-// set up
-background(255);
-size(750, 250);
-smooth();
-frameRate(30);
-strokeWeight(12);
-
-// draw
 function something()
 {
-    stroke(processing.random(50), processing.random(255), processing.random(255), 100);
+    stroke(random(0, 50), random(0, 255), random(0, 255), 100);
     line(i, 0, random(0, width()), height());
     line(width()-i, 0, random(0, width()), height());
     line(i, height(), random(0, width()), 0);
@@ -297,5 +245,5 @@ function something()
   	triangle(0, height(), width(), height(), width()/2, height()/2);
 }
 
-var id = cs1SetInterval(something, 10);
+var id = cs1SetInterval(something, 1000);
 */
