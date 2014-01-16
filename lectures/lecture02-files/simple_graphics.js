0527cc91c9f2bf75d2d2214fa6988d96bf14b29f
legoodfellow <legoodfellow@gmail.com>
2014-01-15 23:56:21 -0500

    Updated simple_graphics to be compatible with the canvas

--- lectures/lecture02-files/simple_graphics.js
+++ lectures/lecture02-files/simple_graphics.js
@@ -1,7 +1,7 @@
-// We must make sure we have all the right wrapper functions for this to work:
-// clear and draw_circle
-    
-clear();
-    
-draw_circle(125, 100, 50);
-draw_circle(275, 100, 50);
+// Draw two circles onto the canvas.
+size(250,300);
+
+fill(0,0,0);
+
+ellipse(100, 100, 50, 50);
+ellipse(175, 100, 50, 50);
