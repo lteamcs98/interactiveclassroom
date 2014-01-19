ee17c5212fcf8231227f2c680b1ede0f124eb5cc
tevnpowers <tevnpowers@gmail.com>
2014-01-19 11:56:45 -0500

    code snippets now use code mirror editors...added edit button to code snippets

--- js/graphics-canvas.js
+++ js/graphics-canvas.js
@@ -2,9 +2,43 @@
  * Manages communication between CodeMirror console and HTML5 canvas
  */
 
+ 
 var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("editor"), {
     lineNumbers: true,
     mode: "text/javascript",
     matchBrackets: true,
     firstLineNumber: 0
 });
+
+var codeSnippetsCodeMirror = CodeMirror.fromTextArea(document.getElementById("codeSnippet"), {
+    lineNumbers: true,
+    mode: "text/javascript",
+    matchBrackets: true,
+    readOnly: "nocursor",
+    firstLineNumber: 0
+});
+
+function codeSnippet(id)
+{
+    var codeSnippetEditor = CodeMirror.fromTextArea(document.getElementById(id), {
+        lineNumbers: true,
+        mode: "text/javascript",
+        matchBrackets: true,
+        readOnly: "nocursor",
+        firstLineNumber: 0
+    });
+
+    $("#" + id + "Edit").click(function() {
+      myCodeMirror.setValue(codeSnippetEditor.getValue());
+    }); 
+}
+
+function outputSnippet(id)
+{
+    CodeMirror.fromTextArea(document.getElementById(id), {
+        mode: "text/javascript",
+        matchBrackets: true,
+        readOnly: "nocursor",
+        firstLineNumber: 0
+    });
+}
