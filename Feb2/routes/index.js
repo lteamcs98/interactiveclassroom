1824bfb25d67c9ff035dea47bb24d4ccee09057d
Michelle Shu <simplevity@gmail.com>
2014-02-02 14:43:13 -0500

    first node app commit

---
+++ Feb2/routes/index.js
@@ -0,0 +1,27 @@
+
+/*
+ * GET home page.
+ */
+
+exports.client = function(req, res) {
+	res.sendfile(__dirname + '/client.html');
+}
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.helloworld = function(req, res){
+    res.render('helloworld', { title: 'Hello, World!' });
+};
+
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({}, {}, function(e, docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
