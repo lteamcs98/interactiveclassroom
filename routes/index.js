a9a8aa37af1a35ce0c0ff66c2ff0f6d24914e9b2
Michelle Shu <simplevity@gmail.com>
2014-02-05 21:40:55 -0500

    adding challengelist back into routes

--- routes/index.js
+++ routes/index.js
@@ -16,3 +16,14 @@ exports.userlist = function(db) {
 exports.challenge = function(req, res) {
 	res.render('challenge', {id : req.params.id});
 };
+
+exports.challengelist = function(db) {
+    return function(req, res) {
+        var collection = db.get('challengecollection');
+        collection.find({}, {}, function(e, docs){
+            res.render('challengelist', {
+                "challengelist" : docs
+            });
+        });
+    };
+};
