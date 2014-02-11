70c3cb81e3ce36d88ee41ff09d79bda140ca8bd7
harth <harth234@gmail.com>
2014-02-10 20:48:43 -0500

    testing user code

--- challenge1sol.js
+++ challenge1sol.js
@@ -7,3 +7,28 @@ var findMax = function(numbArray)
 	}
     return max;
 }
+
+function findMaxFive(numbArray)
+{
+	var maxArray = [];
+	var curMinMax;
+
+	for (var j = 0; j < 5; j++)
+	{
+		var curMax = 0;
+
+		for (var i = 0; i < numbArray.length; i++)
+		{
+			if (numbArray[i] > curMax)
+			{
+				if (j > 0 && numbArray[i] < curMinMax)
+					curMax = numbArray[i];
+				else if (j == 0)
+					curMax = numbArray[i];
+			}
+		}
+		curMinMax = curMax;
+		maxArray[j] = curMax;
+	}
+    return maxArray;
+}
