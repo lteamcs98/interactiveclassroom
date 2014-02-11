325a33a29caabc2eaf58b2c0e8a35a43ce8c3ec3
Eva Xiao <eva@localhost.localdomain>
2014-02-10 20:32:25 -0500

    Updated statistics.js so ou can see the percentage of students who failed X tests (or none)

--- statistics.js
+++ statistics.js
@@ -7,64 +7,47 @@ exports.resultsCalculateMean = resultsCalculateMean;
 exports.resultsCalculateMean = resultsCalculateMedian;
 
 //Kind of my "main" function
-MongoClient.connect('mongodb://Michelle:michelle@ds027769.mongolab.com:27769/heroku_app21896193', function(err, db) {
+MongoClient.connect('mongodb://Michelle:michelle@ds027769.mongolab.com:27769/heroku_app21896193', function main(err, db) {
 	if (err){
 		return console.dir(err);
 	}
 
-  	getResultsByChallenge(db, 1, resultsCalculateMedian); 
+  	var cursor = getResultsByChallenge(db, 1); 
+
+	var testarray = ['0', '1'];
+
+	resultsPercentageFailedTests(db, cursor, testarray);
+	
 });
 
 
 //Simply finds the results or mongodb documents for a certain challenge
-function getResultsByChallenge(db, challengeId, callback) {
+function getResultsByChallenge(db, challengeId) {
 	var collection = db.collection('resultscollection');
-	var cursor = collection.find({"challenge_id": challengeId}, {"score": 1,"user_id":1, "failed": 1, "_id":0});//.addOption(DBQuery.Option.noTimeout); <= addOption isn't the right function for Node.js mongodb driver..will look this up later
-	
-	callback(db, cursor);
+	var cursor = collection.find({"challenge_id": challengeId}, 
+			{"score": 1,"user_id":1, "failed": 1, "_id":0});//.addOption(DBQuery.Option.noTimeout); <= addOption isn't the right function for Node.js mongodb driver..will look this up later
+	return cursor;
 };
 
-//Calculates percentage of students who got a 100%. Will update this to allow you to query for percentage of students that failed 
-//XYZ tests (or the percentage of students who got 100% if failedIndices == null)
-function resultsPercentageCorrect(db, cursor, failedIndices){
+
+function resultsPercentageFailedTests(db, cursor, failedIndices){
 	var total = 0;
 	var correct = 0;
 
 	cursor.toArray(function(err, items) {
 		if (err) throw err;
-
+		
 		for (var i = 0; i < items.length; i++){
-			if (items[i].score == 100) {
+			console.log(items[i]);
+			if (arraysEqual(items[i].failed, failedIndices)){
 				correct += 1;
 			}
 			total += 1;
 		}
 		console.log(correct/total);
+		
 	});
 }
-
-/*
-function resultsPercentageCorrect2(db, cursor, failedIndices){
-	var total = 0;
-	var correct = 0;
-
-	cursor.toArray(function(err, items) {
-		if (err) throw err;
-
-		for (var i = 0; i < items.length; i++){
-			if (failedIndices == "null" && items[i].score == 100) {
-				correct += 1;
-			}
-			else {
-				for (var j = 0; j < failedIndices.length; j++) {
-						
-			}
-			total += 1;
-		}
-		console.log(correct/total);
-	});
-}*/
-
 //Calculates median score for a certain challenge
 function resultsCalculateMedian(db, cursor){
 	var scores = new Array();
@@ -104,4 +87,19 @@ function resultsCalculateMean(db, cursor){
 
 		console.log(total/numScores);
 	});
+}
+
+/*Credit to Stack Overflow*/
+function arraysEqual(a, b) {
+	if (a === b) return true;
+	if (a == null || b == null) return false;
+	if (a.length != b.length) return false;
+
+	// If you don't care about the order of the elements inside
+	// the array, you should sort both arrays here.
+
+	for (var i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
 }
