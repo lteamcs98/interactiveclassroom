3215dd935f64d53e67a3361ae7c2c01d04ccc174
tevnpowers <tevnpowers@gmail.com>
2014-02-24 21:30:55 -0500

    about to do some major merging

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var error = require('../public/js/errorcheck.js');
 var parser = require('../public/js/JSON_parse.js');
+var hash = require('../public/js/hash.js');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
@@ -94,27 +95,38 @@ exports.deletechallenge = function(db) {
 }
 
 exports.addchallenge = function(db, fs, yaml) {
-    return function(req, res) {
-
-        // Print to console the contents of user uploaded challenge.
+	return function(req, res) {
+		// Print to console the contents of user uploaded challenge.
         fs.readFile(req.files.userChallenge.path, 'utf8', function(err, data) {
-          if (err) throw err;
-           console.log("Content of " + req.files.userChallenge.path + ":");
-           var msg = error.uploadErrorCheck(data);
-	   if (msg == true) {
-		res.render('newchallenge', {"errorMsg": "Challenge successfully added!"});
-	   	var JSON = yaml.loadFront(data);
-           	db.get('challengecollection').insert(JSON);
-	   }
-	   else {
-           	res.render('newchallenge', {"errorMsg": msg} );
-	   }
-        });
-
-
-        // If it worked, set the header so the address bar doesn't still say /adduser
-        //res.location("challengelist");
-        // Add forward to success page
-        //res.redirect("challengelist");
-    }
+		if (err) throw err;
+		console.log("Content of " + req.files.userChallenge.path + ":");
+		var msg = error.uploadErrorCheck(data);
+		if (msg == true)
+		{
+			var JSON = yaml.loadFront(data);
+			var promise = db.get('challengecollection').insert(JSON);
+			function getHash(err, doc)
+			{
+					var id_string = new String(doc._id);
+					id_string = id_string.concat(doc.title);
+					id_string = id_string.concat(doc.problem);
+					var code = Math.abs(id_string.hashCode());
+					doc.challengeId = code;
+					console.log('New Document: ', doc);
+					var htmlSnippet = '<iframe src=' + '"interactiveclassroom.herokuapp.com/challenge/' + doc.challengeId.toString() + '"></iframe>';
+					console.log('in function html snippet: ', code, htmlSnippet);
+					res.render('newchallenge', {"errorMsg": "Challenge successfully added!", "htmlSnippet": htmlSnippet});
+			}
+			promise.on('complete', getHash);
+		}
+		else 
+		{
+			res.render('newchallenge', {"errorMsg": msg} );
+		}
+	});
+	// If it worked, set the header so the address bar doesn't still say /adduser
+	//res.location("challengelist");
+	// Add forward to success page
+	//res.redirect("challengelist");
+	}
 }
