5125e8b97f879e3174e2d1480f107fc0282e987d
Michelle Shu <shu.michelle.w@gmail.com>
2014-04-07 19:20:41 -0400

    passport routes

--- routes/passport.js
+++ routes/passport.js
@@ -3,31 +3,11 @@ var Account = require('../models/account');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-		res.render('index', { user : req.user });
-	});
-
-	app.get('/register', function(req, res) {
-		res.render('register', {});
-	});
-
-	app.post('/register', function(req, res) {
-		Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-			if(err) {
-				return res.render('register', { account : account });
-			}
-
-			passport.authenticate('local')(req, res, function() {
-				res.redirect('/');
-			});
-		});
+		res.render('login');
 	});
 
 	app.get('/login', function(req, res) {
-		res.render('login', { user : req.user });
-	});
-
-	app.post('/login', passport.authenticate('local'), function(req, res) {
-		res.redirect('/');
+		res.render('login');
 	});
 
 	app.get('/auth/google', passport.authenticate('google'));
