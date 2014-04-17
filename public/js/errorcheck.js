43ecc5729f4ff548f934de370086e46c8e0b9652
Eva Xiao <eva@localhost.localdomain>
2014-04-16 20:27:34 -0400

    Edited challenge.js and errorcheck.js and added testchallenge.json. Testchallenge.json is essentially the JSON file in our mongodb. The others are edited to accept one single JSON file (until I merge my stuff with Laura's) and errorchecking has been removed currently...still working on a bug.

--- public/js/errorcheck.js
+++ public/js/errorcheck.js
@@ -3,13 +3,17 @@
 module.exports.uploadErrorCheck = uploadErrorCheck;
 
 function uploadErrorCheck(data){ //data is one JSON object
-	var fields = ["title", "problem", "solutions", "functionHeaders", "functionNames", "inputArray"];
+	var fields = ["title", "challengeId", "problem", "functionHeaders", "functionNames", "inputArray", "outputArray"];
 	var missing = new Array();
 	var errorMsg = "Missing the following essential fields: ";
 	var counter = 0;
-
+	var data = JSON.stringify(data);
+	eval("var data = " + data);
+	console.log("from errocheck.js:" + data);
 	for (var i = 0; i < fields.length; i++){ //doc or array of documents
-		if ((fields[i] in data) == false){
+		var field = fields[i];
+		if (data.field == undefined){
+			console.log("from errorcheck.js:" + data.field);
 			missing[counter] = fields[i];
 			counter+= 1;
 		}
--- routes/challenge.js
+++ routes/challenge.js
@@ -120,59 +120,57 @@ module.exports = function(app, fs, yaml)
 			// Print to console the contents of user uploaded challenge.
 			fs.readFile(req.files.userChallenge.path, 'utf8', function(err, data) {
 				if (err) throw err;
-				var mdDocs = parser.parseMarkdown(data);
-				console.log(mdDocs);
+				eval("var data = " + data);
 				var docs_id = new Array();
 				var htmlSnippets = new Array();
-				for (var i = 0; i < mdDocs.length; i++)
-				{
-					//console.log(mdDocs[i]);
-					var msg = error.uploadErrorCheck(mdDocs[i]);
-					if (msg == true)
-					{
-						var JSON = yaml.loadFront(mdDocs[i]);
-						//var promise = db.get('challengecollection').insert(JSON);
-						var newChallenge = new Challenge({"challengeId" : JSON.challengeId, "problem" : JSON.problem, "functionNames" : JSON.functionNames, "inputArray" : JSON.inputArray, "outputArray" : JSON.outputArray, "title" : JSON.title });
-						newChallenge.save();
-						console.log('NEW CHALLENGE CREATED!', newChallenge);
-						updateID(newChallenge);
-						console.log('NEW CHALLENGE UPDATED!', newChallenge);
-						renderTemplate();
 
-						function updateID(doc)
-						{
-								var id_string = new String(doc._id);
-								id_string = id_string.concat(doc.title);
-								id_string = id_string.concat(doc.problem);
-								var code = Math.abs(id_string.hashCode());
-								doc.challengeId = code;
-								docs_id.push(code);
-								console.log(docs_id);
-								doc.update({ '_id': doc._id }, { 'title': doc.title, 'challengeId' : code, 'problem' : doc.problem, 'functionNames' : doc.functionNames, 'inputArray' : doc.inputArray, 'outputArray' : doc.outputArray });
-								console.log(doc);
-								//var update_promise = db.get('challengecollection').update( { _id: doc._id }, { title: doc.title, challengeId : code, problem: doc.problem, functionNames: doc.functionNames, inputArray: doc.inputArray, outputArray: doc.outputArray });
-								//update_promise.on('complete', renderTemplate);
-								//console.log('New Document: ', doc);
-						}
-						function renderTemplate()
-						{
-							console.log('Challenge IDs: ', docs_id);
-							var htmlSnippet = '<iframe src=' + '"http://interactiveclassroom.herokuapp.com/challenge/' + docs_id[htmlSnippets.length] + '"></iframe>';
-							htmlSnippets.push(htmlSnippet);
-							console.log('hello world!', htmlSnippets.length , mdDocs.length);
-							if(htmlSnippets.length == mdDocs.length)
-							{
-								console.log('snippets: ', htmlSnippets);
-								res.render('newchallenge', {"errorMsg": "Challenge successfully added!!!", "iframes": htmlSnippets});
-							}
-						}
-						//promise.on('complete', updateID);
-					}
-					else
+				var msg = error.uploadErrorCheck("" + data);
+				console.log(data.challengeId);
+				console.log(data.problem);
+				//if (msg == true) At the moment, errorchecking has bug where JSON is a JS Object, not a JSON object
+				//{
+					//var promise = db.get('challengecollection').insert(JSON);
+					var newChallenge = new Challenge({"challengeId" : data.challengeId, "problem" : data.problem, "functionNames" : data.functionNames, "inputArray" : data.inputArray, "outputArray" : data.outputArray, "title" : data.title, "functionHeaders": data.functionHeaders });
+					newChallenge.save();
+					//console.log('NEW CHALLENGE CREATED!', newChallenge);
+					updateID(newChallenge);
+					console.log('NEW CHALLENGE UPDATED!', newChallenge);
+					renderTemplate();
+
+					function updateID(doc)
 					{
-						res.render('newchallenge', {"errorMsg": msg, "iframes": new Array() } );
+							var id_string = new String(doc._id);
+							id_string = id_string.concat(doc.title);
+							id_string = id_string.concat(doc.problem);
+							var code = Math.abs(id_string.hashCode());
+							doc.challengeId = code;
+							docs_id.push(code);
+							//console.log(docs_id);
+							doc.update({ '_id': doc._id }, { 'title': doc.title, 'challengeId' : code, 'problem' : doc.problem, 'functionNames' : doc.functionNames, 'inputArray' : doc.inputArray, 'outputArray' : doc.outputArray, 'functionHeaders' : doc.functionHeaders });
+							console.log(doc);
+							//var update_promise = db.get('challengecollection').update( { _id: doc._id }, { title: doc.title, challengeId : code, problem: doc.problem, functionNames: doc.functionNames, inputArray: doc.inputArray, outputArray: doc.outputArray });
+							//update_promise.on('complete', renderTemplate);
+							//console.log('New Document: ', doc);
 					}
-				}
+					function renderTemplate()
+					{
+						console.log('Challenge IDs: ', docs_id);
+						var htmlSnippet = '<iframe src=' + '"http://interactiveclassroom.herokuapp.com/challenge/' + docs_id[htmlSnippets.length] + '"></iframe>';
+						htmlSnippets.push(htmlSnippet);
+						console.log('hello world!', htmlSnippets.length , data.length);
+						//if(htmlSnippets.length == data.length) *** Commented out until data is an array
+						//{
+							console.log('snippets: ', htmlSnippets);
+							res.render('newchallenge', {"errorMsg": "Challenge successfully added!!!", "iframes": htmlSnippets});
+						//}
+					}
+					//promise.on('complete', updateID);
+				//}
+				//else
+				//{
+				//	res.render('newchallenge', {"errorMsg": msg, "iframes": new Array() } );
+				//}
+				
 			});
 		}
 	}
