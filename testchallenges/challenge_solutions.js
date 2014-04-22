f5d545a189acb5bbdadd4493af27f9ed578d0d8a
Eva Xiao <eva@localhost.localdomain>
2014-04-21 20:22:34 -0400

    Added updated challenge solutions to testchallenges dir

---
+++ testchallenges/challenge_solutions.js
@@ -0,0 +1,113 @@
+var findMax = function(numbArray)
+{
+	var max = numbArray[0];
+	for (var i = 0; i < numbArray.length; i++)
+	{
+		if (numbArray[i] > max) max = numbArray[i];
+	}
+	return max;
+};
+
+function findMaxFive(numbArray)
+{
+	var maxArray = [];
+	var curMinMax;
+
+	for (var j = 0; j < 5; j++)
+	{
+		var curMax = 0;
+
+		for (var i = 0; i < numbArray.length; i++)
+		{
+			if (numbArray[i] > curMax)
+			{
+				if (j > 0 && numbArray[i] < curMinMax)
+					curMax = numbArray[i];
+				else if (j == 0)
+					curMax = numbArray[i];
+			}
+		}
+		curMinMax = curMax;
+		maxArray[j] = curMax;
+	}
+    return maxArray;
+}
+
+function findMax(numbArray)
+{
+	var max = numbArray[0];
+	for (var i = 0; i < numbArray.length; i++)
+	{
+		if (numbArray[i] > max) max = numbArray[i];
+	}
+	return max;
+}
+
+
+function getSquare(someNumber)
+{
+	return someNumber*someNumber;
+}
+
+function squareList(numbers)
+{
+	for(i in numbers)
+	{
+		numbers[i] = getSquare(numbers[i]);
+	}
+	return numbers;
+}
+
+function findMaxFive(numbArray)
+{
+	var maxArray = [];
+	var curMinMax;
+
+	for (var j = 0; j < 5; j++)
+	{
+		var curMax = 0;
+
+		for (var i = 0; i < numbArray.length; i++)
+		{
+			if (numbArray[i] > curMax)
+			{
+				if (j > 0 && numbArray[i] < curMinMax)
+					curMax = numbArray[i];
+				else if (j == 0)
+					curMax = numbArray[i];
+			}
+		}
+		curMinMax = curMax;
+		maxArray[j] = curMax;
+	}
+    return maxArray;
+}
+
+function mergesort(values)
+{
+    // Terminal case: 0 or 1 item arrays don't need sorting
+    if (values.length < 2) {
+        return values;
+    }
+    var middle = Math.floor(values.length / 2),
+        left    = values.slice(0, middle),
+        right   = values.slice(middle);
+
+    return merge(mergesort(left), mergesort(right));
+}
+
+function merge(left, right)
+{
+    var result  = [],
+        il      = 0,
+        ir      = 0;
+
+    while (il < left.length && ir < right.length){
+        if (left[il] < right[ir]){
+            result.push(left[il++]);
+        } else {
+            result.push(right[ir++]);
+        }
+    }
+    return result.concat(left.slice(il)).concat(right.slice(ir));
+}
